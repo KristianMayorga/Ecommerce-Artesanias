c3d136182f3ec8911b1160ec1375a6dd5d8085f7
miticold25 <michelldiazf07@gmail.com>
2024-11-06 16:10:52 -0600

    Update views.py

--- backend/CarroCompra/views.py
+++ backend/CarroCompra/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from productos.models import Producto
+from .models import CarroCompra
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse 
 
-# Create your views here.
+@login_required
+
+def agregar_producto_carrito(request, producto_id, cantidad=1):
+
+    # Producto en la bd
+    producto = get_object_or_404(Producto, id=producto_id)
+
+    # Carrito del usuario
+    carrito, created = CarroCompra.objects.get_or_create(cliente=request.user.cliente)
+
+    try:
+        carrito.agregar_producto(producto, cantidad)
+        return JsonResponse({'success': True, 'message': 'Producto agregado al carrito con éxito.'})
+    
+    except ValueError as e:
+        # Si no hay suficiente stock
+        return JsonResponse({'success': False, 'message': 'No hay suficiente stock para este producto.'}, status=400)
+    
