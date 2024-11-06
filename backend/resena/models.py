9ffd2a3273b1d96dcb169d3f5cbdd8bcdeaff0c8
miticold25 <michelldiazf07@gmail.com>
2024-11-06 15:04:06 -0600

    Update models.py

--- backend/resena/models.py
+++ backend/resena/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.conf import settings
-
+from productos.models import Producto
+from accounts.models import Usuario
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 class Resena(models.Model):
-    id = models.AutoField(primary_key=True)
-    producto = models.ForeignKey('Producto', on_delete=models.CASCADE, related_name='resenas')
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='resenas')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name='resenas')
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     contenido = models.TextField()
-    calificacion = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])  # Rango de 1 a 5
+    calificacion = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     fecha = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = 'resena'
+        constraints = [
+            models.UniqueConstraint(fields=['producto', 'usuario'], name='unique_resena_producto_usuario')
+        ]
