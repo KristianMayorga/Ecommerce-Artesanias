42f0e82f4f7a52aa1929b1c6f9a365f2aac2f541
miticold25 <michelldiazf07@gmail.com>
2024-11-13 23:23:09 -0600

    Update admin.py

--- backend/productos/admin.py
+++ backend/productos/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Producto, Resena
+from .models import Producto 
 
 admin.site.register(Producto)
-admin.site.register(Resena)
+
