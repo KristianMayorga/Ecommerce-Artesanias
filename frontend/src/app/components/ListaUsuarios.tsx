bafdc8d48afd3cd143567f93c8851b1d5785aeb2
Fer <mfperezh24@gmail.com>
2025-02-09 11:54:38 -0500

    Update ListaUsuarios.tsx

--- frontend/src/app/components/ListaUsuarios.tsx
+++ frontend/src/app/components/ListaUsuarios.tsx
@@ -440,6 +440,7 @@ export default function UserManagement() {
                                     type="date"
                                     value={formData.dateOfBirth}
                                     onChange={handleInputChange}
+                                    max={new Date(new Date().setFullYear(new Date().getFullYear() - 18)).toISOString().split('T')[0]}
                                     required
                                     className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-600"
                                 />
